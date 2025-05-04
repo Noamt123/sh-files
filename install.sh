b87efd845aea75667cda70d8b2136e98fb7a0108
Noam Tuchman <51915849+Noamt123@users.noreply.github.com>
2025-05-03 19:46:55 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo apt-get install curl tilix xterm
+sudo apt-get install linux-tools-generic linux-cloud-tools-generic
+sudo apt-get install linux-tools-$(uname -r) linux-cloud-tools-$(uname -r)
+sudo apt-get install -y build-essential libelf-dev libcap2 libcap-dev binutils-dev
+sudo apt-get install -y make gcc libncurses-dev flex bison build-essential bc libssl-dev libelf-dev dwarves zstd jq
+
+sudo apt install -y python3 python3-pip python3-setuptools git ninja-build
+
+sudo apt-get install -y tilix xterm
+sudo apt update
+sudo apt install --reinstall gnome-terminal
+
