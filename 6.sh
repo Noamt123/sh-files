9792ddb2ff6c30820916836dd79bc308a163a37e
Noam Tuchman <51915849+Noamt123@users.noreply.github.com>
2025-05-03 19:49:52 -0600

    Create 6.sh

---
+++ 6.sh
@@ -0,0 +1,8 @@
+sudo apt install software-properties-common
+
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+
+sudo apt install python3.11
+sudo apt install python3.11-venv python3.11-distutils
+curl -sS https://bootstrap.pypa.io/get-pip.py | sudo python3.11
